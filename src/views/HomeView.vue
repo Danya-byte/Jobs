9c914825ebac66253f53189f84e1856c1c9a4820
Ivashin <danya001406@gmail.com>
2025-03-01 21:40:35 +0300

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@
     <div class="content">
         <div class="categories">
             <button class="category-btn active">Jobs</button>
-            <RouterLink to="/nft">
+            <RouterLink to="#">
                 <button class="category-btn">Gift</button>
             </RouterLink>
         </div>
