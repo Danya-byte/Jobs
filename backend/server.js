e8b7cc1de7c526fdb0ea65ef7784bebb10baa318
Ivashin <danya001406@gmail.com>
2025-03-02 03:43:31 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const crypto = require('crypto');
 const axios = require('axios');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
 
 const BOT_TOKEN ="7745513073:AAHn69lYwvKBx-gXn7fKBevLnnDZzXp1lrY";
 const CLOUDPUB_URL = 'https://impotently-dutiful-hare.cloudpub.ru';
