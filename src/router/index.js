63411126ba0c3378886adda1e220e4282f9e63c3
Ivashin <danya001406@gmail.com>
2025-03-16 18:43:00 +0300

    Chat fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import ChatView from './views/ChatView.vue';
-import ChatListView from './views/ChatListView.vue';
-import HomeView from './views/HomeView.vue';
-import ProfileView from './views/ProfileView.vue';
-
-
-const routes = [
-  {
-    path: '/',
-    component: HomeView,
-    name: 'home'
-  },
-  {
-    path: '/chats',
-    component: ChatListView,
-    name: 'chat-list'
-  },
-  {
-    path: '/chat/:chatId',
-    component: ChatView,
-    props: true,
-    name: 'chat',
-    beforeEnter: (to, from, next) => {
-      const chatId = to.params.chatId;
-      if (!chatId || !chatId.includes('_')) {
-        console.warn('Invalid chatId format. Expected: jobId_targetUserId');
-        next('/chats');
-      } else {
-        next();
-      }
-    }
-  },
-  {
-    path: '/profile/:userId',
-    component: ProfileView,
-    name: 'profile',
-    props: true
-  }
-];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/ProfileView.vue'),
+    },
+    {
+      path: '/profile/:userId',
+      name: 'profileWithId',
+      component: () => import('../views/ProfileView.vue'),
+      props: (route) => ({
+        userId: route.params.userId,
+        username: route.query.username,
+      }),
+    },
+    {
+      path: '/nft',
+      name: 'nft',
+      component: () => import('../views/NftView.vue'),
+    },
+    {
+      path: '/chat/:chatId',
+      name: 'chat',
+      component: () => import('../views/ChatView.vue'),
+      props: (route) => {
+        const chatId = route.params.chatId;
+        const [jobId, targetUserId] = chatId.split('_');
+        return {
+          jobId,
+          targetUserId,
+          chatId,
+          username: route.query.username,
+        };
+      },
+      beforeEnter: (to, from, next) => {
+        const chatId = to.params.chatId;
+        if (!chatId || !chatId.includes('_')) {
+          console.warn('Invalid chatId format. Expected: jobId_targetUserId');
+          next({ name: 'chatList' });
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: '/chats',
+      name: 'chatList',
+      component: () => import('../views/ChatListView.vue'),
+    },
+  ],
 });
 
 export default router;
