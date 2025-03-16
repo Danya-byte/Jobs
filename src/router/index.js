3db064af60d384489420ea02a3dbdd6c181bff22
Ivashin <danya001406@gmail.com>
2025-03-16 18:41:02 +0300

    Chat fix

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,39 @@ import ChatListView from './views/ChatListView.vue';
 import HomeView from './views/HomeView.vue';
 import ProfileView from './views/ProfileView.vue';
 
+
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/chats', component: ChatListView },
-  { path: '/chat/:chatId', component: ChatView, props: true },
-  { path: '/profile/:userId', component: ProfileView },
+  {
+    path: '/',
+    component: HomeView,
+    name: 'home'
+  },
+  {
+    path: '/chats',
+    component: ChatListView,
+    name: 'chat-list'
+  },
+  {
+    path: '/chat/:chatId',
+    component: ChatView,
+    props: true,
+    name: 'chat',
+    beforeEnter: (to, from, next) => {
+      const chatId = to.params.chatId;
+      if (!chatId || !chatId.includes('_')) {
+        console.warn('Invalid chatId format. Expected: jobId_targetUserId');
+        next('/chats');
+      } else {
+        next();
+      }
+    }
+  },
+  {
+    path: '/profile/:userId',
+    component: ProfileView,
+    name: 'profile',
+    props: true
+  }
 ];
 
 const router = createRouter({
